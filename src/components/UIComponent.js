a10e173c0fccd3160bd9dca24cc1616f42b1fb24
caizx <saas_plat@163.com>
2019-11-29 21:31:57 +0800

    修改可以不在container中render

--- src/components/UIComponent.js
+++ src/components/UIComponent.js
@@ -2,19 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UIRender from './UIRender';
 
-export default class UIComponent extends React.Component{
+export default class UIComponent extends React.Component {
   static propTypes = {
     config: PropTypes.object.isRequired,
   }
 
   static contextTypes = {
-    onEvent: PropTypes.func.isRequired,
-    t: PropTypes.func.isRequired,
+    onEvent: PropTypes.func,
+    t: PropTypes.func,
+  }
+
+  onEvent = (...args) => {
+    if (this.context.onEvent) {
+      this.context.onEvent(...args);
+    }
+  }
+
+  t = (txt)=>{
+    if (this.context.t){
+      this.context.t(txt);
+    }
+    return txt;
   }
 
   state = {}
 
-  renderItem(config){
+  renderItem(config) {
     return <UIRender key={config.key} ui={config}/>
   }
 

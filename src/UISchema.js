d6de4fe6c6d709ca537e00a36a928f46ba511b0e
caizx <saas_plat@163.com>
2019-12-18 23:30:46 +0800

    补充对象和数组的操作函数

--- src/UISchema.js
+++ src/UISchema.js
@@ -1,7 +1,33 @@
 import {
-  assignId
-} from './util';
+  toJS,
+  observable,
+} from "mobx";
+import _isPlainObject from 'lodash/isPlainObject';
 import UIStore from './UIStore';
+
+let gid = 0;
+// 分配全局id
+const assignId = (pre) => {
+  // 每次加一
+  gid = gid + 1;
+  return (pre || '') + gid;
+}
+
+const createArgs = (val) => {
+  return Object.keys(val).reduce((obj, key) => {
+    let subexpr = UIStore.parseExpr(val[key]);
+    Object.defineProperty(obj, key, {
+      enumerable: true, // 这里必须是可枚举的要不observable不好使
+      get: () => {
+        return this.store.execExpr(subexpr);
+      },
+      set: (expr) => {
+        subexpr = UIStore.parseExpr(expr);
+      }
+    });
+    return obj;
+  }, {});
+}
 
 export default class UISchema {
   type;
@@ -18,47 +44,118 @@ export default class UISchema {
   createModel(store, reducer) {
     const key = assignId(this.type);
     const ukeys = Object.keys(this.props);
+    const bkey = this.bind;
     let obj = ukeys.reduce((obj, key) => {
-      const setkey = 'set' + key.substr(0, 1).toUpperCase() + key.substr(1);
-      let valexpr = UIStore.parseExpr(this.props[key]);
-      Object.defineProperty(obj, key, {
-        enumerable: true, // 这里必须是可枚举的要不observable不好使
-        get: () => {
-          if (this.bind) {
-            const bindval = store.getViewModel(this.bind + '.' + key);
-            if (bindval) {
-              return bindval;
+      const upkey = key.substr(0, 1).toUpperCase() + key.substr(1);
+      const setkey = 'set' + upkey;
+      const val = this.props[key];
+      if (typeof val === 'string') {
+        let valexpr = UIStore.parseExpr(val);
+        Object.defineProperty(obj, key, {
+          enumerable: true, // 这里必须是可枚举的要不observable不好使
+          get: () => {
+            if (bkey) {
+              const bindval = store.getViewModel(bkey + '.' + key);
+              if (bindval) {
+                return bindval;
+              }
             }
+            return store.execExpr(valexpr);
+          },
+          set: (value) => {
+            if (this[setkey]) {
+              return store.setViewModel(this[setkey], value);
+            }
+            valexpr = UIStore.parseExpr(value);
           }
-          return store.execExpr(valexpr);
-        },
-        set: (value) => {
-          if (this[setkey]) {
-            return store.setViewModel(this[setkey], value);
+        });
+      } else if (_isPlainObject(val)) {
+        const skey = '_' + key;
+        obj[skey] = observable.map();
+        this[skey].merge(createArgs(val));
+        Object.defineProperty(obj, key, {
+          enumerable: true, // 这里必须是可枚举的要不observable不好使
+          get: () => {
+            if (bkey) {
+              const bindval = store.getViewModel(bkey + '.' + key);
+              if (bindval) {
+                return bindval;
+              }
+            }
+            return this[skey].toJSON();
+          },
+          set: (value) => {
+            if (this[setkey]) {
+              return store.setViewModel(this[setkey], value);
+            }
+            const obj = createArgs(value);
+            this[skey].merge(obj);
           }
-          valexpr = UIStore.parseExpr(value);
+        });
+        obj['remove' + upkey] = (...names) => {
+          if (this['set' + upkey]) {
+            const per = this['set' + upkey];
+            for (const name of names) {
+              store.setViewModel(per + (per ? '.' : '') + name, undefined);
+            }
+            return
+          }
+          for (const name of names) {
+            this[skey].delete(name);
+          }
         }
-      });
+        obj['clear' + upkey] = () => {
+          if (this['set' + upkey]) {
+            const per = this['set' + upkey];
+            const args = store.execExpr(per);
+            const names = Object.keys(toJS(args));
+            for (const name of names) {
+              store.setViewModel(per + (per ? '.' : '') + name, undefined);
+            }
+            return
+          }
+          this[skey].clear();
+        }
+      } else {
+        Object.defineProperty(obj, key, {
+          enumerable: true, // 这里必须是可枚举的要不observable不好使
+          get: () => {
+            if (bkey) {
+              const bindval = store.getViewModel(bkey + '.' + key);
+              if (bindval) {
+                return bindval;
+              }
+            }
+            return val;
+          },
+          set: (value) => {
+            if (this[setkey]) {
+              return store.setViewModel(this[setkey], value);
+            }
+            this.props[key] = value;
+          }
+        });
+      }
       return obj;
     }, {});
 
     // 需要把bind字段视图状态字段生成出来
-    if (this.bind) {
-      const subvm = store.getViewModel(this.bind);
+    if (bkey) {
+      const subvm = store.getViewModel(bkey);
       obj = Object.keys(subvm).filter(key => ukeys.indexOf(key) === -1).reduce((obj, key) => {
         Object.defineProperty(obj, key, {
           enumerable: true, // 这里必须是可枚举的要不observable不好使
           get: () => {
-            return store.getViewModel(this.bind + '.' + key);
+            return store.getViewModel(bkey + '.' + key);
           },
           set: (value) => {
-            return store.setViewModel(this.bind + '.' + key, value);
+            return store.setViewModel(bkey + '.' + key, value);
           }
         });
         return obj;
       }, obj);
     }
-    return {
+    return observable({
       get store() {
         return store;
       },
@@ -69,8 +166,27 @@ export default class UISchema {
         return this.name || key
       },
       ...obj,
-      items: this.items.map(reducer)
-    }
+      _items: this.items.map(reducer),
+      get items() {
+        return this._items.filter(it => it.visible)
+      },
+      addItem: (...items) => {
+        this._items.push(...items);
+      },
+      removeItem: (...names) => {
+        for (const name of names) {
+          const reit = this._items.find(it => it.name === name);
+          if (reit) {
+            this._items.splice(this._items.indexOf(reit), 1);
+          } else {
+            console.warn('uimodel items not exists!', name);
+          }
+        }
+      },
+      clearItems: () => {
+        this._items.clear();
+      }
+    })
   }
 
   static createSchema({

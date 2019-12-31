24d5d90e3750d198b0b55deb64068e49aed63dd9
caizx <caizx@chanjet.com>
2019-12-31 10:11:48 +0800

    修改tree模型

--- src/models/TreeModel.js
+++ src/models/TreeModel.js
@@ -8,5 +8,37 @@ import TableModel from './TableModel';
 
 
 export default class TreeModel extends TableModel{
- 
+  // data = [
+  //  {
+  //    title: 'Node1',
+  //    value: '0-0',
+  //    key: '0-0',
+  //    children: [    // <============ 子树
+  //      {
+  //        title: 'Child Node1',
+  //        value: '0-0-1',
+  //        key: '0-0-1',
+  //      },
+  //      {
+  //        title: 'Child Node2',
+  //        value: '0-0-2',
+  //        key: '0-0-2',
+  //      },
+  //    ],
+  //  },
+  //  {
+  //    title: 'Node2',
+  //    value: '0-1',
+  //    key: '0-1',
+  //  },
+  // ]
+  constructor(store, {
+    data = [],
+    ...props
+  }) {
+    super(store, {
+      data,
+      ...props
+    });
+  }
 }

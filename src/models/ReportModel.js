7391a5eac34051fc42391b839560c80eb1701e91
caizx <caizx@chanjet.com>
2020-01-10 17:06:26 +0800

    定义报表模型

--- src/models/ReportModel.js
+++ src/models/ReportModel.js
@@ -1,8 +1,48 @@
+import {
+  computed,
+  action,
+} from 'mobx';
 import Model from './Model';
 
 // 报表图表
 export default class ReportModel extends Model {
 
- 
+  @computed get headerRowCount(){
 
+  }
+
+  @computed get columnCount(){
+
+  }
+
+  @computed get fixedColumnCount(){
+    if (this.fixedIndex === undefined){
+      return 0;
+    }
+    return this.fixedIndex + 1;
+  }
+
+  getColumnWidth(columnIndex){
+
+  }
+
+  getColumn(rowIndex, columnIndex){
+
+  }
+
+  getCell(rowIndex, columnIndex){
+
+  }
+
+  isRowLoaded(rowIndex){
+
+  }
+
+  @action loadData(rowIndex, size){
+
+  }
+
+  @action selectCell(rowIndex, columnIndex){
+
+  }
 }

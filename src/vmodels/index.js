11bc77bdc96c4ce187058b8de0a9884fd3816b33
caizx <caizx@saas-plat.com>
2020-05-04 10:14:10 +0800

    修改名称错误

--- src/vmodels/index.js
+++ src/vmodels/index.js
@@ -1,7 +1,7 @@
 import FreeView from './FreeView';
 import Voucher from './Voucher';
 import EditList from './EditList';
-import VoucherList from './VoucherList';
+import List from './List';
 
 export default {
   // 增加一个自定义模板，里面什么也没有预制需要开发者自己控制
@@ -9,5 +9,5 @@ export default {
   // 单据模型
   Voucher,
   EditList,
-  VoucherList
+  List
 }
